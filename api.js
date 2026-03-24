aa8d0b601ae937b58484bcef558a26cb4e3acf29
Anthonyr1616 <anthonyr1616@gmail.com>
2026-03-24 18:27:12 -0400

    Add method for getting vocab by levels and add vocab class

--- api.js
+++ api.js
@@ -1,11 +1,39 @@
 // WaniKani API documentation: https://docs.api.wanikani.com/20170710/#getting-started
 
-
-function GetApiTokenFromCache() {
-    return localStorage.getItem('apiToken');
+function getApiTokenFromCache() {
+  return localStorage.getItem("apiToken");
 }
 
-function SetApiTokenInCache(token) {
-    localStorage.setItem('apiToken', token);
+function setApiTokenInCache(token) {
+  localStorage.setItem("apiToken", token);
 }
 
+async function getVocabByLevelWindow(levels) {
+  const apiToken = getApiTokenFromCache();
+  let url = `https://api.wanikani.com/v2/subjects?types=vocabulary&levels=${levels.join(",")}`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${apiToken}`,
+    },
+  });
+
+  const data = await response.json(); 
+
+  return data.data.map((vocabData) => new Vocab(vocabData));
+}
+
+class Vocab {
+  constructor(jsonData) {
+    this.id = jsonData.id;
+    this.level = jsonData.data.level;
+    this.characters = jsonData.data.characters;
+    this.meanings = jsonData.data.meanings.map((m) => m.meaning);
+    this.readings = jsonData.data.readings.map((r) => r.reading);
+    this.partsOfSpeech = jsonData.data.parts_of_speech;
+    this.contextSentences = jsonData.data.context_sentences.map((s) => ({
+      english: s.en,
+      japanese: s.ja,
+    }));
+  }
+}
